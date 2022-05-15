4a62f692314cd3dc78b1eccec8e20505ee331a3e
Mina <minaghozati@gmail.com>
2022-05-14 22:32:05 -0700

    add new component called hello world

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+    render() {
+        return (
+            <div>
+                <p>hello world</p>
+            </div>
+        );
+    }
+}
+
+export default Test;
