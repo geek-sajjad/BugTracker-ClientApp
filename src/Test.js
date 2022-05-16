e1bcf199b4d35b5c114f1c9230d87028c30463ff
Mina <minaghozati@gmail.com>
2022-05-16 08:18:10 +0430

    add main part

--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import styles from './Test.module.css';
 
 class Test extends Component {
     render() {
         return (
-            <div>
-                <p>Hi My name is mina ghozati</p>
-            </div>
+            <>
+              <div className={styles.main}>
+                  <div className={styles.chap}>
+                      <button >buttton 1</button>
+                      <button >buttton 1</button>
+                      <button >buttton 1</button>
+                      <button >buttton 1</button>
+                  </div>
+                 <div className={styles.rast}>
+                     <p>container</p>
+                 </div>
+
+                
+                </div>
+            </>
+          
         );
     }
 }
